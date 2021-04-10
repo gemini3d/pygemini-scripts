9c50a4c86b177c7cac527a5a05beb0fcb1dbb975
Matthew D. Zettergren <mattzett@gmail.com>
2021-04-10 12:56:58 -0400

    added some code to extract coefficients from tridiag soln

---
+++ tridiag/coeffs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 12:46:10 2021
+
+Example code to load the parabolic coefficient data into an ipython workspace
+
+@author: zettergm
+"""
+
+from read_coeffs import load_coeffs
+
+filename="/Users/zettergm/Projects/gemini3d/build/src/numerical/diffusion/test_diffusion1d.h5"
+[t,coeffs,rhs]=load_coeffs(filename)
---
+++ tridiag/read_coeffs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 18:24:38 2021
+
+illustrates handling of output coefficient file from geminis parabolic solver.  This
+takes the output of the test_diffusion1D.f90 program and converts it to numpy arrays
+
+@author: zettergm
+"""
+
+# imports
+import numpy as np
+import h5py
+
+
+def load_coeffs(filename):
+    h5file=h5py.File(filename,"r")
+    keylist=list(h5file.keys())
+    lx=h5file[keylist[0]].size
+    
+    # initial cycle through the variables to see how many time steps were output
+    indt=0
+    for key in keylist:
+        if key[0:6]=="coeffs":
+            indt=indt+1
+    
+    # now read time,coefficient, and rhs variables; must convert to numpy arrays in the process
+    t=np.zeros(indt)
+    coeffs=np.zeros((3,lx,indt))
+    rhs=np.zeros((lx,indt))
+    indt=0
+    for key in keylist:
+        if key[0:6]=="coeffs":
+            indt=int(key[6:])-1
+            coeffs[:,:,indt]=np.array(h5file[key])
+        elif key[0:1]=="t":
+            indt=int(key[1:])-1
+            t[indt]=np.array(h5file[key])
+        elif key[0:3]=="rhs":
+            indt=int(key[3:])-1
+            rhs[:,indt]=np.array(h5file[key])
+            
+    return [t,coeffs,rhs]
