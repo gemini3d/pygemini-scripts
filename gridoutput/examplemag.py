3d06e0f6dd1ec6f0261ae583c735c782e4cc632a
Matthew D. Zettergren <mattzett@gmail.com>
2021-06-03 17:52:14 -0400

    plot gridded output data from example

--- gridoutput/examplemag.py
+++ gridoutput/examplemag.py
@@ -10,6 +10,7 @@ Created on Wed May 26 10:34:45 2021
 from gemini3d.grid.gridmodeldata import model2magcoords
 import gemini3d.read
 import matplotlib.pyplot as plt
+import numpy as np
 
 # load some sample data
 direc="~/simulations/tohoku20113D_lowres_2Daxisneu_CI/"
@@ -18,8 +19,34 @@ xg=gemini3d.read.grid(direc)
 dat=gemini3d.read.frame(direc,cfg["time"][-1])
 
 # grid data
-parm=dat["v1"]
+parm=np.array(dat["v1"])
+[alti,mloni,mlati,parmi]=model2magcoords(xg,parm,256,256,256)
 
-[alti,mloni,mlati,parmi]=model2magcoords(xg,parm,100,100,100)
+# define slices indices
+altref=300e3
+ialt=np.argmin(abs(alti-altref))
+lonavg=cfg["sourcemlon"]
+ilon=np.argmin(abs(mloni-lonavg))
+latavg=cfg["sourcemlat"]
+ilat=np.argmin(abs(mlati-latavg))
 
-# plot
+# plot various slices through the 3D domain
+plt.subplots(1,3)
+
+plt.subplot(1,3,1)
+plt.pcolormesh(mloni,alti/1e3,parmi[:,:,ilat])
+plt.xlabel('mlon')
+plt.ylabel('alt')
+plt.colorbar()
+
+plt.subplot(1,3,2)
+plt.pcolormesh(mloni,mlati,parmi[ialt,:,:].transpose())
+plt.xlabel('mlon')
+plt.ylabel('mlat')
+plt.colorbar()
+
+plt.subplot(1,3,3)
+plt.pcolormesh(mlati,alti/1e3,parmi[:,ilon,:])
+plt.xlabel('mlat')
+plt.ylabel('alt')
+plt.colorbar()
--- gridoutput/interpn_test.py
+++ gridoutput/interpn_test.py
@@ -8,6 +8,7 @@ Created on Wed May 26 16:47:57 2021
 
 import numpy as np
 import scipy.interpolate
+import matplotlib.pyplot as plt
 
 x1=np.linspace(-1,1,384)
 x2=np.linspace(-1,1,96)
@@ -15,15 +16,27 @@ x3=np.linspace(-1,1,64)
 
 [X1,X2,X3]=np.meshgrid(x1,x2,x3,indexing="ij")
 values=np.sqrt(X1**2+X2**2+X3**2)
-#x1i=np.linspace(-1.1,1.1,256)
-#x2i=np.linspace(-1.1,1.1,256)
-#x3i=np.linspace(-1.1,1.1,256)
-x1i=np.random.randn(256)
-x2i=np.random.randn(256)
-x3i=np.random.randn(256)
+x1i=np.linspace(-1.1,1.1,256)
+x2i=np.linspace(-1.1,1.1,256)
+x3i=np.linspace(-1.1,1.1,256)
+#x1i=np.random.randn(256)
+#x2i=np.random.randn(256)
+#x3i=np.random.randn(256)
 
 
 [X1i,X2i,X3i]=np.meshgrid(x1i,x2i,x3i,indexing="ij")
 xi=np.array((X1i.flatten(),X2i.flatten(),X3i.flatten())).transpose()
 valuesi=scipy.interpolate.interpn((x1,x2,x3),values,xi,method="linear",bounds_error=False,fill_value=np.NaN)
 valuesi=np.reshape(valuesi,[256,256,256])
+
+plt.subplots(1,2)
+plt.subplot(1,2,1)
+plt.pcolormesh(x1,x2,values[:,:,32].transpose())
+plt.colorbar()
+
+plt.subplot(1,2,2)
+plt.pcolormesh(x1i,x2i,valuesi[:,:,128].transpose())
+plt.colorbar()
+
+plt.show()
+
