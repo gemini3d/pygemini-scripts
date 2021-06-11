635797b945931ae545ea59604e828194d3ea6ffe
Matthew D. Zettergren <mattzett@gmail.com>
2021-06-11 15:45:18 -0400

    improved efficiency of plotting of 3D dipole simulations

--- gridoutput/plotcurv.py
+++ gridoutput/plotcurv.py
@@ -5,52 +5,50 @@
 
 # imports
 from gemini3d.grid.gridmodeldata import model2magcoords
-from matplotlib.pyplot import figure
-import numpy as np
+import matplotlib.pyplot as plt
 import xarray
 
 
 def plotcurv3D(xg, parm: xarray.DataArray, cfg, parmlabel, lalt=256, llon=256, llat=256):
     """plot dipole data vs. alt,lon,lat"""
 
-    # grid data; wasteful and should only do a slice at a time???
-    [alti, mloni, mlati, parmi] = model2magcoords(xg, parm, lalt, llon, llat)
-
     # define slices indices
     altref = 300e3
-    ialt = np.argmin(abs(alti - altref))
-    lonavg = cfg["sourcemlon"]
-    ilon = np.argmin(abs(mloni - lonavg))
-    latavg = cfg["sourcemlat"]
-    ilat = np.argmin(abs(mlati - latavg))
-
-    # plot various slices through the 3D domain
-    fg = figure()
+    
+    fg = plt.figure()
     axs = fg.subplots(1, 3)
-
+    
+    [alti, mloni, mlati, parmi] = model2magcoords(xg, parm, 256, 1, 256, mlonlims=(cfg["sourcemlon"]))    
     ax = axs[0]
-    h=ax.pcolormesh(mlati, alti / 1e3, parmi[:, ilon, :], shading="nearest")
-    ax.set_xlabel("mlat")
-    ax.set_ylabel("alt")
+    h=ax.pcolormesh(mlati, alti / 1e3, parmi[:, 0, :], shading="nearest")
+    ax.set_xlabel("mlat (deg)")
+    ax.set_ylabel("alt (km)")
     cbar=fg.colorbar(h, ax=ax)
     cbar.set_label(parmlabel)
-
+    plt.show(block=False)
+    
+    [alti, mloni, mlati, parmi] = model2magcoords(xg, parm, 1, 256, 256,altlims=(altref))
     ax = axs[1]
-    h = ax.pcolormesh(mloni, mlati, parmi[ialt, :, :].transpose(), shading="nearest")
-    ax.set_xlabel("mlon")
-    ax.set_ylabel("mlat")
+    h = ax.pcolormesh(mloni, mlati, parmi[0, :, :].transpose(), shading="nearest")
+    ax.set_xlabel("mlon (deg)")
+    ax.set_ylabel("mlat (deg)")
     cbar=fg.colorbar(h, ax=ax)
     cbar.set_label(parmlabel)
-
+    plt.show(block=False)
+    
+    [alti, mloni, mlati, parmi] = model2magcoords(xg, parm, 256, 256, 1, mlatlims=(cfg["sourcemlat"]))
     ax = axs[2]
-    h = ax.pcolormesh(mloni, alti / 1e3, parmi[:, :, ilat], shading="nearest")
-    ax.set_xlabel("mlon")
-    ax.set_ylabel("alt")
+    h = ax.pcolormesh(mloni, alti / 1e3, parmi[:, :, 0], shading="nearest")
+    ax.set_xlabel("mlon (deg)")
+    ax.set_ylabel("alt (km)")
     cbar=fg.colorbar(h, ax=ax)
     cbar.set_label(parmlabel)
+    plt.show(block=False)
 
-# alt,lon plot for 2D dipole data
+
 def plotcurv2D(xg, parm, parmlabel,lalt=512, llat=512):
+    """ alt,lon plot for 2D dipole data"""
+    
     # grid data
     [alti, mloni, mlati, parmi] = model2magcoords(xg, parm, lalt, 1, llat)
 
@@ -58,10 +56,10 @@ def plotcurv2D(xg, parm, parmlabel,lalt=512, llat=512):
     ilon = 0
 
     # plot the meridional slice
-    fg = figure()
+    fg = plt.figure()
     ax = fg.gca()
     h = ax.pcolormesh(mlati, alti / 1e3, parmi[:, ilon, :], shading="nearest")
-    ax.set_xlabel("mlat")
-    ax.set_ylabel("alt")
+    ax.set_xlabel("mlat (deg)")
+    ax.set_ylabel("alt (km)")
     cbar=fg.colorbar(h, ax=ax)
     cbar.set_label(parmlabel)
