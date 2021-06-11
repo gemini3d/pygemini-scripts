00cabada13d00b47d1adb89a054b8d7e8846438e
Matthew Zettergren <zettergm@Matthews-Mac-Pro.local>
2021-06-11 09:25:41 -0400

    start dipole plotting work

--- gridoutput/examplemag2D.py
+++ gridoutput/examplemag2D.py
@@ -7,27 +7,17 @@ Created on Wed May 26 10:34:45 2021
 """
 
 # imports
-from gemini3d.grid.gridmodeldata import model2magcoords
 import gemini3d.read
-import matplotlib.pyplot as plt
+from plotcurv import plotcurv2D
 import numpy as np
 
-# load some sample data (3D)
-direc="~/simulations/tohoku20112D_medres/"
+# load some sample data (2D)
+direc="~/simulations/raid/tohoku20112D_medres/"
 cfg=gemini3d.read.config(direc)
 xg=gemini3d.read.grid(direc)
 dat=gemini3d.read.frame(direc,cfg["time"][-1])
 
 # grid data
 parm=np.array(dat["v1"])
-[alti,mloni,mlati,parmi]=model2magcoords(xg,parm,1024,1,1024)
+plotcurv2D(xg,parm,lalt=1024,llat=1024)
 
-# define slices indices
-ilon=0
-
-# plot various slices through the 2D domain
-plt.figure()
-plt.pcolormesh(mlati,alti/1e3,parmi[:,ilon,:])
-plt.xlabel('mlat')
-plt.ylabel('alt')
-plt.colorbar()
--- gridoutput/examplemag3D.py
+++ gridoutput/examplemag3D.py
@@ -7,46 +7,17 @@ Created on Wed May 26 10:34:45 2021
 """
 
 # imports
-from gemini3d.grid.gridmodeldata import model2magcoords
 import gemini3d.read
-import matplotlib.pyplot as plt
+from plotcurv import plotcurv3D
 import numpy as np
 
 # load some sample data (3D)
-direc="~/simulations/tohoku20113D_lowres_2Daxisneu_CI/"
+direc="~/simulations/raid/tohoku20113D_lowres_2Daxisneu/"
 cfg=gemini3d.read.config(direc)
 xg=gemini3d.read.grid(direc)
 dat=gemini3d.read.frame(direc,cfg["time"][-1])
 
 # grid data
 parm=np.array(dat["v1"])
-[alti,mloni,mlati,parmi]=model2magcoords(xg,parm,256,256,256)
+plotcurv3D(xg,parm,cfg,lalt=128,llon=128,llat=128)
 
-# define slices indices
-altref=300e3
-ialt=np.argmin(abs(alti-altref))
-lonavg=cfg["sourcemlon"]
-ilon=np.argmin(abs(mloni-lonavg))
-latavg=cfg["sourcemlat"]
-ilat=np.argmin(abs(mlati-latavg))
-
-# plot various slices through the 3D domain
-plt.subplots(1,3)
-
-plt.subplot(1,3,1)
-plt.pcolormesh(mloni,alti/1e3,parmi[:,:,ilat])
-plt.xlabel('mlon')
-plt.ylabel('alt')
-plt.colorbar()
-
-plt.subplot(1,3,2)
-plt.pcolormesh(mloni,mlati,parmi[ialt,:,:].transpose())
-plt.xlabel('mlon')
-plt.ylabel('mlat')
-plt.colorbar()
-
-plt.subplot(1,3,3)
-plt.pcolormesh(mlati,alti/1e3,parmi[:,ilon,:])
-plt.xlabel('mlat')
-plt.ylabel('alt')
-plt.colorbar()
