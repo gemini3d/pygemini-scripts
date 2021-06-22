25e776ebbf2657e3def7e1c0331d342b433f1af4
Matthew Zettergren <zettergm@Matthews-Mac-Pro.local>
2021-06-22 16:46:17 -0400

    adding gridding example

--- gridoutput/examplemag2D.py
+++ gridoutput/examplemag2D.py
@@ -6,14 +6,21 @@
 import gemini3d.read
 from plotcurv import plotcurv2D
 from matplotlib.pyplot import show
+from gemini3d.grid.gridmodeldata import model2magcoords
 
 # load some sample data (2D)
 direc = "~/simulations/raid/2Dmidlat_test/"
 cfg = gemini3d.read.config(direc)
 xg = gemini3d.read.grid(direc)
-dat = gemini3d.read.frame(direc, cfg["time"][1])
+dat = gemini3d.read.frame(direc, cfg["time"][0])
 
 # grid data
-plotcurv2D(xg, dat["ne"], lalt=1024, llat=1024)
+lalt=1024
+llat=1024
+llon=1
+parm=dat["v1"]
+alti,mloni,mlati,parmi=model2magcoords(xg,parm,lalt,llon,llat)
 
-show()
+# plot data
+plotcurv2D(xg, dat["ne"], lalt=1024, llat=1024)
+show(block=False)
--- tridiag/coeffs.py
+++ tridiag/coeffs.py
@@ -6,6 +6,11 @@ Example code to load the parabolic coefficient data into an ipython workspace
 """
 
 from read_coeffs import load_coeffs
+import matplotlib.pyplot as plt
 
 filename = "~/4096pts.h5"
 t, coeffs, rhs, temperature = load_coeffs(filename)
+
+plt.figure()
+plt.pcolormesh(temperature)
+plt.show(block=False)
