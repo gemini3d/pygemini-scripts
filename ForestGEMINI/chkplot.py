c2f89b221c8531da4c335084d3feefafad266be2
Matthew D. Zettergren <mattzett@gmail.com>
2022-11-28 20:37:44 -0500

    add script to make basic forestgemini plot

---
+++ ForestGEMINI/chkplot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 17:34:24 2022
+
+@author: zettergm
+"""
+
+import gemini3d.read
+import matplotlib.pyplot as plt
+
+direc="~/simulations/tohoku20113D_lowres_axineu_fclaw_dneu/"
+filename="20110311_35900.000000.h5"
+dat=gemini3d.read.data(direc+filename)
+v1=dat["v1"][:,:,24]
+
+plt.figure(dpi=200)
+plt.pcolormesh(v1)
+plt.colorbar()
+plt.show()
+
