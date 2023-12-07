5fe20732bafdc897655ea42c9846b581c735b2ed
Matthew Zettergren <zettergm@Matthews-MacBook-Pro.local>
2023-12-07 16:09:13 -0500

    update to lagrangian testing scripts again

--- plotGDI/chk_lagrangian.py
+++ plotGDI/chk_lagrangian.py
@@ -9,16 +9,26 @@ Created on Fri Dec  1 19:40:12 2023
 import gemini3d.read
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interpn
 
-flagpot=False
+flagpot=True           # plot potential
+flagframeshift=True    # shift quantities into same FOR
+
+# information about the ExB drift (or grid drift as the case may be)
+v2grid=900
+v3grid=0
 
 parmlbl="J2"
 altref=[90e3,115e3,300e3,600e3,800e3]
 
+
+# Read data
 if not ("xg" in locals()):
     print("Reloading data...")
-    direc="~/simulations/ssd/200km_lagrangian6/"
-    direc2="~/simulations/ssd/200km_eulerian3/"
+#    direc="~/simulations/ssd/200km_lagrangian8/"
+#    direc2="~/simulations/ssd/200km_eulerian3/"
+    direc="~/simulations/ssd/200km_lagrangian_lowres3/"
+    direc2="~/simulations/ssd/200km_eulerian_lowres3/"
     cfg=gemini3d.read.config(direc)
     xg=gemini3d.read.grid(direc)
     x=xg["x2"][2:-2]
@@ -28,38 +38,72 @@ if not ("xg" in locals()):
     x2=xg2["x2"][2:-2]
     y2=xg2["x3"][2:-2]
     z2=xg2["x1"][2:-2]
-    it=5
+    it=1
     #dat=gemini3d.read.frame(direc,cfg["time"][it],var=parmlbl)
     #dat2=gemini3d.read.frame(direc2,cfg["time"][it],var=parmlbl)   
     dat=gemini3d.read.frame(direc,cfg["time"][it])
-    dat2=gemini3d.read.frame(direc2,cfg["time"][it])   
+    dat2=gemini3d.read.frame(direc2,cfg["time"][it])
+
+
+# Apply FOR xform by shifting the Eulerian coordinates
+print("Interpolating parameter of interest to a common grid...")
+dataplot=np.array(dat[parmlbl])
+dataplot2=np.array(dat2[parmlbl])
+t=(cfg["time"][it]-cfg["time"][0]).total_seconds()
+x2shift=x2-v2grid*t
+y2shift=y2-v3grid*t
+[Z,X,Y]=np.meshgrid(np.array(z),np.array(x),np.array(y),indexing="ij")   # interpolation sites are based on Lagrangian grid
+srcpoints=(np.array(z),np.array(x2shift),np.array(y2shift))
+targetpoints=(Z.flatten(order="F"), X.flatten(order="F"), Y.flatten(order="F"))
+dataplot2interp=interpn(srcpoints,dataplot2,targetpoints,bounds_error=False,fill_value=np.nan)
+dataplot2interp=dataplot2interp.reshape(dataplot.shape,order="F")
+    
 
 # Compare the requested parameter
-plt.subplots(len(altref),2,dpi=150)
+plt.subplots(len(altref),3,dpi=150)
 for ialt in range(0,len(altref)):
     iz=np.argmin(abs(z-altref[ialt]))
-    plt.subplot(len(altref),2,2*ialt+1)
-    plt.pcolormesh(x,y,dat[parmlbl][iz,:,:])
+    plt.subplot(len(altref),3,3*ialt+1)
+    plt.pcolormesh(x,y,dataplot[iz,:,:].transpose())
     plt.colorbar()
     if (ialt==0):
         plt.title(str(cfg["time"][it]))
     iz2=np.argmin(abs(z2-altref[ialt]))
-    plt.subplot(len(altref),2,2*ialt+2)
-    plt.pcolormesh(x2,y2,dat2[parmlbl][iz2,:,:])
+    plt.subplot(len(altref),3,3*ialt+2)
+    plt.pcolormesh(x2,y2,dataplot2interp[iz2,:,:].transpose())
+    plt.colorbar()
+    if (ialt==0):
+        plt.title(parmlbl)
+    plt.subplot(len(altref),3,3*ialt+3)
+    plt.pcolormesh(x,y,(dataplot[iz,:,:]-dataplot2interp[iz2,:,:]).transpose())
     plt.colorbar()
     if (ialt==0):
         plt.title(parmlbl)
     plt.show()
 
 
+# Interpolate potential as well
+print("Resampling potential pattern...")
+Phiplot=np.array(dat["Phitop"])
+Phiplot2=np.array(dat2["Phitop"])
+[X,Y]=np.meshgrid(np.array(x),np.array(y),indexing="ij")   # interpolation sites are based on Lagrangian grid
+srcpoints=(np.array(x2shift),np.array(y2shift))
+targetpoints=(X.flatten(order="F"), Y.flatten(order="F"))
+Phiplot2interp=interpn(srcpoints,Phiplot2,targetpoints,bounds_error=False,fill_value=np.nan)
+Phiplot2interp=Phiplot2interp.reshape(Phiplot.shape,order="F")
+
+
 # Also compare potential solutions
 if flagpot: 
-    plt.subplots(1,2,dpi=150)
-    plt.subplot(1,2,1)
-    plt.pcolormesh(dat["Phitop"])
+    plt.subplots(1,3,dpi=150)
+    plt.subplot(1,3,1)
+    plt.pcolormesh(Phiplot)
     plt.colorbar()
-    plt.subplot(1,2,2)
-    plt.pcolormesh(dat2["Phitop"])
+    plt.subplot(1,3,2)
+    plt.pcolormesh(Phiplot2interp)
+    plt.colorbar()
+    plt.subplot(1,3,3)
+    plt.pcolormesh(Phiplot-Phiplot2interp)
     plt.colorbar()
     plt.show()
 
--- plotGDI/chk_pot_sources.py
+++ plotGDI/chk_pot_sources.py
@@ -9,9 +9,17 @@ Created on Tue Dec  5 14:24:29 2023
 import gemini3d.read
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interpn
 
-direc="~/simulations/ssd/200km_lagrangian5/"
-direc2="~/simulations/ssd/200km_eulerian3/"
+
+# information about the ExB drift (or grid drift as the case may be)
+v2grid=900
+v3grid=0
+
+
+# Load the grid and configurations
+direc="/Users/zettergm/simulations/ssd/200km_lagrangian_lowres4/"
+direc2="/Users/zettergm/simulations/ssd/200km_eulerian_lowres4/"
 cfg=gemini3d.read.config(direc)
 xg=gemini3d.read.grid(direc)
 x=xg["x2"][2:-2]
@@ -23,37 +31,69 @@ x2=xg2["x2"][2:-2]
 y2=xg2["x3"][2:-2]
 z2=xg2["x1"][2:-2]
 
+
 # check source terms and potential solves for each
 lx=xg["lx"]
 lxtwo=xg2["lx"]
-
-fname="/Users/zettergm/simulations/ssd/200km_lagrangian5/Jdebug.dat"
+fname=direc+"/Jdebug.dat"
 fdata=np.fromfile(fname,dtype="float64",count=lx[0]*lx[1]*lx[2]*2)
 fdata=fdata.reshape((lx[0],lx[1],lx[2],2),order="F")
 J2=fdata[:,:,:,0]
 J3=fdata[:,:,:,1]
-
-fname2="/Users/zettergm/simulations/ssd/200km_eulerian3/Jdebug.dat"
+fname2=direc2+"/Jdebug.dat"
 fdata2=np.fromfile(fname2,dtype="float64",count=lxtwo[0]*lxtwo[1]*lxtwo[2]*2)
 fdata2=fdata2.reshape((lxtwo[0],lxtwo[1],lxtwo[2],2),order="F")
 J22=fdata2[:,:,:,0]
 J32=fdata2[:,:,:,1]
 
 
+# do the frame of reference shift for the Eulerian data
+print("Interpolating parameter of interest to a common grid...")
+J22plot=np.array(J22)
+J32plot=np.array(J32)
+t=2
+x2shift=x2-v2grid*t
+y2shift=y2-v3grid*t
+[Z,X,Y]=np.meshgrid(np.array(z),np.array(x),np.array(y),indexing="ij")   # interpolation sites are based on Lagrangian grid
+srcpoints=(np.array(z),np.array(x2shift),np.array(y2shift))
+targetpoints=(Z.flatten(order="F"), X.flatten(order="F"), Y.flatten(order="F"))
+J22plotinterp=interpn(srcpoints,J22plot,targetpoints,bounds_error=False,fill_value=np.nan)
+J22plotinterp=J22plotinterp.reshape(J2.shape,order="F")
+J32plotinterp=interpn(srcpoints,J32plot,targetpoints,bounds_error=False,fill_value=np.nan)
+J32plotinterp=J32plotinterp.reshape(J3.shape,order="F")
+
+
 # plot debug info
+ialt=20;
 plt.subplots(2,2,dpi=150)
 plt.subplot(2,2,1)
-plt.pcolormesh(x,y,J2[20,:,:])
+plt.pcolormesh(x,y,J2[ialt,:,:].transpose())
+plt.title("J2")
 plt.colorbar()
 
 plt.subplot(2,2,2)
-plt.pcolormesh(x,y,J3[20,:,:])
+plt.pcolormesh(x,y,J3[ialt,:,:].transpose())
+plt.title("J3")
 plt.colorbar()
 
 plt.subplot(2,2,3)
-plt.pcolormesh(x2,y2,J22[20,:,:])
+plt.pcolormesh(x2,y2,J22plotinterp[ialt,:,:].transpose())
 plt.colorbar()
 
 plt.subplot(2,2,4)
-plt.pcolormesh(x2,y2,J32[20,:,:])
+plt.pcolormesh(x2,y2,J32plotinterp[ialt,:,:].transpose())
 plt.colorbar()
+
+
+plt.subplots(1,2,dpi=150)
+plt.subplot(1,2,1)
+plt.pcolormesh(x,y,(J2[ialt,:,:]-J22plotinterp[ialt,:,:]).transpose())
+plt.title("dJ2")
+plt.colorbar()
+
+plt.subplot(1,2,2)
+plt.pcolormesh(x,y,(J3[ialt,:,:]-J32plotinterp[ialt,:,:]).transpose())
+plt.title("dJ3")
+plt.colorbar()
+
+
