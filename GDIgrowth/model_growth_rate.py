d98068ef86727208203d1595adfd8a1cf458010d
zettergm <zettergm@zettergms-MacBook-Pro.local>
2026-06-30 09:55:53 -0400

    slight updates to growth rate script

--- GDIgrowth/model_growth_rate.py
+++ GDIgrowth/model_growth_rate.py
@@ -16,11 +16,16 @@ import matplotlib.pyplot as plt
 parmlbl="ne"
 
 # User-specified values based on initial simulation frame inspection
-x0=-655e3            # initial x-position of trailing edge of patch
-z0=312e3             # hmF2
+#x0=-655e3            # initial x-position of trailing edge of patch
+#z0=312e3             # hmF2
+x0=-85e3
+z0=300e3
 
 # Simulation information and grid
-direc="~/simulations/sdcard/ONR_Lamarche/GDI_reference/"
+#direc="~/simulations/sdcard/GDI_periodic_lowres/"
+#direc="~/simulations/sdcard/GDI_periodic_lowres_denspot/"
+#direc="~/simulations/sdcard/ONR_Lamarche/GDI_reference/"
+direc="~/simulations/sdcard/GDI_periodic_lowres_denspot_1e-6/"
 #direc="~/simulations/sdcard/ONR_Lamarche/GDI_reference_hires/"
 cfg=gemini3d.read.config(direc)
 xg=gemini3d.read.grid(direc)
@@ -36,6 +41,8 @@ ly=xg["lx"][2]
 
 # Initial patch position index
 ix0=np.argmin(abs(x-x0))
+iz=np.argmin(abs(z-z0))
+iy0=ly//2
 
 # Organize timing info
 lt=len(cfg["time"])      # number of simulation output files for this run
@@ -50,7 +57,7 @@ parm=np.empty((lz,lx,ly,lt))
 tsec=np.empty((lt))
 for it in range(0,lt):
     print(cfg["time"][it])
-    dat=gemini3d.read.frame(direc,cfg["time"][it],var=parmlbl)
+    dat=gemini3d.read.frame(direc,cfg["time"][it],var=[parmlbl,"v2"])
     parm[:,:,:,it]=dat[parmlbl]
     tsec[it]=(cfg["time"][it]-refdate).total_seconds()
 
@@ -58,16 +65,20 @@ for it in range(0,lt):
         v = -cfg["Eyit"]/abs(Bmag)
         
 # time series analysis for growth rate
-iz=np.argmin(abs(z-z0))
 parmxyt=parm[iz,:,:,:]
 parmyt=np.empty((ly,lt))
 parmamp=np.empty((lt))
 xprime=np.empty((lt))
+ne0=np.empty((lt))
 for it in range(0,lt):
     xprime[it]=x0+v*tsec[it]           # position of gradient translated to current time
     ix=np.argmin(abs(x-xprime[it]))
     parmyt[:,it]=parmxyt[ix,:,it]
     parmamp[it]=np.std(parmxyt[ix,:,it])
+    ne0[it]=np.mean(parmxyt[ix,:,it])
+#    Gives same results
+#    meanparm=np.mean(parmxyt[ix,:,it])
+#    parmamp[it]=np.max(parmxyt[ix,:,it]-meanparm)
 
 # Plot a curve that represents the growth of the fluctuation amplitude
 plt.figure()
@@ -77,15 +88,18 @@ plt.ylabel("amplitude")
 
 # Define a saturation value (with some buffer) and remove elements from the saturated state
 satamp=parmamp[-1]
-amplimit=0.75*satamp    # linear growth up to some percentage of the saturated state
+#amplimit=0.75*satamp    # linear growth up to some percentage of the saturated state
+amplimit=0.5*satamp    # linear growth up to some percentage of the saturated state
 inds=parmamp<amplimit
 tsec_nosat=tsec[inds]
 parmamp_nosat=parmamp[inds]
 
 # Find the time of local minimum and remove anything up to that time
 itmin=np.argmin(parmamp_nosat)
-t_linear=tsec_nosat[itmin:]
-parm_linear=parmamp_nosat[itmin:]
+#it0=itmin      # 1 min. output
+it0=itmin+3    # 10s output
+t_linear=tsec_nosat[it0:]
+parm_linear=parmamp_nosat[it0:]
 
 # plot just the "linear" regime, as well as the saturation amplitude used
 plt.semilogy(t_linear,parm_linear)
@@ -100,20 +114,46 @@ logslope=p[0]
 parmfit=np.exp(p[1])*np.exp(logslope*t_linear)
 
 # analytical form of growth rate
-# colden=np.empty((lx,ly,lt))
-# for ix in range(0,lx):
-#     for iy in range(0,ly):
-#         colden[ix,iy,it]=np.trapz(parm[:,ix,iy,itmin],z)
-#         dNdx,dNdy=np.gradient(colden,x,y)
+colden=np.empty((lx,ly))
+for ix in range(0,lx):
+    for iy in range(0,ly):
+        colden[ix,iy]=np.trapz(parm[:,ix,iy,it0],z)
+        
+dNdx,dNdy=np.gradient(colden,x,y)
 
-# Initial patch position index, but take from after when simulation has settled
-ix0_itmin=np.argmin(abs(x-xprime[itmin]))
-parmslice=parm[iz,:,ly//2,itmin]
-dndx=np.gradient(parmslice,x)
-ell=parmslice[ix0_itmin]/dndx[ix0_itmin]
-gamma=v/ell
+ell=colden/dNdx
+ix0_it0=np.argmin(abs(x-xprime[it0]))
+#gamma=v/ell[ix0_itmin,iy0]
+gamma=v/np.mean(ell[ix0_it0,:])
 
-plt.plot(t_linear,parmfit,'.')
-plt.legend( ("amplitude", "linear stage", "saturated reference", "log-linear fit") )
+# # Initial patch position index, but take from after when simulation has settled
+# ix0_itmin=np.argmin(abs(x-xprime[itmin]))
+# parmslice=parm[iz,:,ly//2,itmin]
+# dndx=np.gradient(parmslice,x)
+# ell=parmslice[ix0_itmin]/dndx[ix0_itmin]
+# gamma=v/ell
+
+plt.plot(t_linear,parmfit,'^')
+plt.plot(tsec,ne0,"--")
+plt.plot(tsec,0.1*ne0,":")
+plt.legend( ("amplitude", "linear stage", "saturated reference", "log-linear fit","background","nonlinearity") )
 plt.title("Emprical $\gamma$ = "+str(logslope)+"; Analytical $\gamma$ = "+str(gamma) )
 
+# recompute the conductivities
+import gemini3d.conductivity
+import os
+os.environ["GEMINI_ROOT"]="~/Projects/gemini3d/build/_deps/msis-build/"
+dat=gemini3d.read.frame(direc,cfg["time"][0])
+sigP,sigH,_,_,_,_,_ = gemini3d.conductivity.conductivity_reconstruct(refdate, dat, cfg, xg)
+
+#plt.figure()
+plt.subplots(1,2)
+plt.subplot(1,2,1)
+plt.semilogx(parm[:,ix0,iy0,0],z/1e3)
+plt.xlabel("plasma density (m$^{-3}$)")
+plt.ylabel("altitud (km)")
+plt.subplot(1,2,2)
+plt.semilogx(sigP[:,ix0,iy0],z/1e3,abs(sigH[:,ix0,iy0]),z/1e3)
+plt.legend(("Pedersen","Hall"))
+plt.xlabel("conductance (mhos)")
+plt.ylabel("altitud (km)")
